f1de222c200ec70112e503c046b34537df26c661
Sevda Ağaç <sevdakarapolatt@gmail.com>
2025-04-08 18:44:09 +0300

    add Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+
 const ojsNutLinks = [
   { name: "İletişim", path: "/iletisim" },
   { name: "Hakkımızda", path: "/hakkimizda" },
@@ -10,6 +11,7 @@ const ojsNutLinks = [
   { name: "Gerçek Müşteri Yorumları" },
   { name: "Blog" }
 ];
+
 const categories = [
   { name: "Whey Protein", path: "#" },
   { name: "Cream of Rice", path: "#" },
@@ -21,6 +23,7 @@ const categories = [
   { name: "Günlük Vitamin Paketi", path: "#" },
   { name: "ZMA", path: "#" }
 ];
+
 const popularProducts = [
   { name: "Protein", path: "#" },
   { name: "Spor Gıdaları", path: "#" },
@@ -36,18 +39,18 @@ const popularProducts = [
 const Footer = () => {
   return (
     <footer className="border-t bg-[#000000] py-8 text-white">
-      <div className="max-w-screen-xl mx-auto my-10">
+      <div className="container mx-auto px-4 md:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-8 pt-5 md:grid-cols-2">
-          <h5>
+          <h5 className="text-lg">
             "LABORATUVAR TESTLİ ÜRÜNLER"
             <br />
             AYNI GÜN & ÜCRETSİZ KARGO <br />
             MEMNUNİYET GARANTİSİ
           </h5>
-          <p>
+          <p className="text-gray-300">
             200.000'den fazla ürün yorumumuza dayanarak, ürünlerimizi
             seveceğinize eminiz. Eğer herhangi bir sebeple memnun kalmazsan,
-            bizimle iletişime geçtiğinde çözüme kavuşturacağız.
+            bizimle iletişime geçtiğinde çözüme kavuşturacağız.
           </p>
         </div>
         <div className="grid grid-cols-1 gap-8 pt-28 md:grid-cols-3">
@@ -59,31 +62,40 @@ const Footer = () => {
                 <br />
                 NUTRITION
               </h4>
-              
-              <ul className="space-y-1 text-gray-300 ">
-               {ojsNutLinks.map((ojs)=>
-              <li key={ojs.name}>{ojs.name}</li>
-              )}
+              <ul className="space-y-2 text-gray-300">
+                {ojsNutLinks.map((ojs) => (
+                  <li key={ojs.name}>
+                    <Link to={ojs.path || "#"} className="hover:text-white">
+                      {ojs.name}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
           <div>
             <h4 className="mb-2 font-bold">Kategoriler</h4>
-            <ul className="space-y-1 text-gray-300">
-              {categories.map((category)=>
-             <li key={category.name}>{category.name}</li>
-             
-             )}
+            <ul className="space-y-2 text-gray-300">
+              {categories.map((category) => (
+                <li key={category.name}>
+                  <Link to={category.path} className="hover:text-white">
+                    {category.name}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
-
           <div>
-          <h4 className="mb-2 font-bold">Popüler Ürünler</h4>
-          <ul className="space-y-1 text-gray-300">
-         {popularProducts.map((product)=>
-       <li key={product.name}>{product.name}</li>
-       )}
-        </ul>
+            <h4 className="mb-2 font-bold">Popüler Ürünler</h4>
+            <ul className="space-y-2 text-gray-300 leading-relaxed">
+              {popularProducts.map((product) => (
+                <li key={product.name}>
+                  <Link to={product.path} className="hover:text-white">
+                    {product.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
         <div className="mt-8 border-t border-gray-200 pt-8 text-center">
